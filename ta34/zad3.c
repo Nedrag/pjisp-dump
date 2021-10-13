aeaa3f7fa61a50225bd173251be92b8b3809f606
Nedrag <92254461+Nedrag@users.noreply.github.com>
2021-10-13 10:49:01 +0200

    git commit

--- ta34/zad3.c
+++ ta34/zad3.c
@@ -1,5 +1,32 @@
 #include <stdio.h>
-
+#define MAX_SIZE 20
 int main(){
-    printf("Hello World");
+   int a[5] = {2, 1, 4, 3, 5}; 
+   int i ,n , b;
+   n = sizeof(a)/sizeof(a[0]);
+
+   int j, min_indx, temp;
+
+   for(i = 0; i < n - 1 ; i++){
+       min_indx = i;
+       for( j = i + 1; j < n;j++){
+           if(a[min_indx] < a[j]){
+               min_indx = j;
+            }
+        }
+        temp = a[i];
+        a[i] = a[min_indx];
+        a[min_indx] = temp;
+    }
+    printf("[ ");
+    for(b = 0; b < n; b++){
+        if( b > 0){
+            printf(",");
+        }
+        printf("%d", a[b]);
+    }
+    printf("]");
+
+    
+
 }
--- ta34/zad4.c
+++ ta34/zad4.c
@@ -1,3 +1,59 @@
+#include <stdio.h>
+
+
+#define MAX_SIZE 20
+
+
+
 int main(){
-    int a = 7;
+    int i, n, a[MAX_SIZE];
+
+    do{
+        printf("Unesite broj elemenata niza: ");
+        scanf("%d", &n);
+    }while(n >= MAX_SIZE || n < 0);
+
+    for(i = 0; i < n; i++){
+        printf("Niz[%d] = ", i);
+        scanf("%d", &a[i]);
+
+    }
+    printf("[ "); 
+    
+    for(i =0 ; i < n; i++){
+        if(i > 0){
+            printf(",");
+        }
+        printf("%d", a[i]);
+
+
+    }
+    printf(" ]");
+
+    int min_inx, j, temp;
+
+    for(i = 0; i < n-1; i++){
+        min_inx = i;
+        for( j = i +1; j < n; j++){
+            if(a[min_inx] < a[j]){
+                min_inx = j;
+            }
+
+        }
+        temp = a[i];
+        a[i] = a[min_inx];
+        a[min_inx] = temp;
+    }
+    printf("[ "); 
+    
+    for(i =0 ; i < n; i++){
+        if(i > 0){
+            printf(",");
+        }
+        printf("%d", a[i]);
+
+
+    }
+    printf(" ]");
 }
+
